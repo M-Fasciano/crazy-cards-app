036bbe75f0a1811a3bec5c6e01eab26ec1b2e015
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2022-08-09 19:04:46 +0100

    Spelling fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import GloblalStyle from "./Components/Global/CSS";
+import GlobalStyle from "./Components/Global/CSS";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <>
-    <GloblalStyle />
+    <GlobalStyle />
     <App />
   </>
 );
